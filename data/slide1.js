916288f2239d7b7f5e64fd0bdd9c80b1bb8b0f05
nguyenchimnpl <90503949+nguyenchimnpl@users.noreply.github.com>
2021-11-14 00:10:43 +0700

    acc

---
+++ data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{53DA717A-A7CC-4F3D-9225-D819C2F7204C}'];loadHandler&&loadHandler(0, '<div id="spr0_db54d8cc"><div id="spr1_db54d8cc" class="kern slide"><img id="img0_db54d8cc" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_db54d8cc" class="kern slide"><div id="vd0_db54d8cc"></div></div></div>');})();
